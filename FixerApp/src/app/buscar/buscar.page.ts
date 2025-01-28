6620da7d7567a55a02eb64363bca64241b90d738
pablomn02 <pablomnavarro9@gmail.com>
2025-01-29 00:44:51 +0100

    Buscador de la pagina principal implementado

--- FixerApp/src/app/buscar/buscar.page.ts
+++ FixerApp/src/app/buscar/buscar.page.ts
@@ -8,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarPage implements OnInit {
 
+  palabras: string[] = [
+    'Limpieza', 
+    'Pintura', 
+    'Electricista', 
+    'Carpintería', 
+    'Fontanería', 
+    'Jardinería', 
+    'Cerrajería', 
+    'Albañilería', 
+    'Decoración', 
+    'Mudanzas', 
+    'Reparación de electrodomésticos', 
+    'Mantenimiento de piscinas', 
+    'Cuidado de mascotas', 
+    'Clases particulares', 
+    'Reparación de bicicletas'
+  ];
+    palabraBuscada: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.palabraBuscada = this.palabras;
   }
 
+
+  // Funcion de busqueda de servicios
+  searchBar(event: any) {
+    const text = event.target.value.toLowerCase().trim();
+    if (text && text !== '') {
+      this.palabraBuscada = this.palabras.filter((palabra) => {
+        return palabra.toLowerCase().includes(text);
+      });
+    } else {
+      this.palabraBuscada = this.palabras;
+    }
+  }
 }
